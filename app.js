d60dd33358c22bab5c4e8027c4d309b730f1d91d
Aditya Inapurapu <adityaii@gmail.com>
2024-03-08 17:54:35 -0800

    yfi

--- app.js
+++ app.js
@@ -6,8 +6,19 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const cors = require('cors')
 
 var app = express();
+let corsOptions = {
+  origin: ['*'],
+}
+
+app.use(cors(corsOptions))
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +34,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,10 @@
 var express = require('express');
+var axios = require('axios');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  map = { "hello": "world" }
-  res.json(map);
+router.get('/', function (req, res, next) {
+  axios.get("https://query1.finance.yahoo.com/v8/finance/chart/AAPL").then(response => res.json(response.data));
 });
 
 module.exports = router;
